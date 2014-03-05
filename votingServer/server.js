76ce4fb40e5798b1a6d0bb0555af78533c0031b1
Craig Peterson <cpeterson@ancestry.com>
2014-03-05 13:27:40 -0700

    actual voting data server running.

--- votingServer/server.js
+++ votingServer/server.js
@@ -1,6 +1,8 @@
 
 var thrift = require('thrift');
 
+var voteCounts = {};
+
 var votingService = require('./gen-nodejs/VotingService'),
     ttypes = require('./gen-nodejs/vote_types');
 	
@@ -11,11 +13,14 @@ var votingService = require('./gen-nodejs/VotingService'),
 		},
 		PlaceVote: function(vote,result){
 			console.log('vote for ' + vote);
+			if(!voteCounts[vote])voteCounts[vote] = 0
+			voteCounts[vote]++
 			result(null);
 		},
 		GetVoteCounts: function(result){
-			result(null,{"C#":42,"java":13})
-		}
+			result(null,voteCounts)
+		},
+		
 	}
 	
 var server_http = thrift.createHttpServer(votingService,handler, {protocol: thrift.TJSONProtocol});
