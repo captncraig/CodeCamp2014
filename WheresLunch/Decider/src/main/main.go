7c8ca61b70581931b055f2bd407db182abc0e39d
Craig Peterson <cpeterson@ancestry.com>
2014-03-10 15:47:23 -0600

    Cleaning everything else out.

--- WheresLunch/Decider/src/main/main.go
+++ WheresLunch/Decider/src/main/main.go
@@ -2,22 +2,8 @@ package main
 
 import(
 "fmt"
-"thrift"
-"lunch"
 )
 
 func main(){
-	
-	transport,err := thrift.NewTSocket("localhost:9011")
-	if err != nil{
-		fmt.Println(err)
-	}
-	defer transport.Close()
-	if err := transport.Open(); err != nil {
-               fmt.Println(err)
-    }
-	
-	client := lunch.NewLunchServiceClientFactory(transport,thrift.NewTCompactProtocolFactory())
-	result, _ := client.MakeDecision();
-	fmt.Printf("Eat at: %s!",result)
+	fmt.Println("Hello, World")
 }
