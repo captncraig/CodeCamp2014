242549368d24170926cf08a326473e89eeef907e
Craig Peterson <cpeterson@ancestry.com>
2014-03-14 11:57:41 -0600

    Go Client

--- WheresLunch/Decider/src/main/main.go
+++ WheresLunch/Decider/src/main/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"lunch"
+	"thrift"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	transport, _ := thrift.NewTSocket("localhost:9011")
+
+	defer transport.Close()
+	transport.Open()
+
+	client := lunch.NewLunchServiceClientFactory(transport, thrift.NewTCompactProtocolFactory())
+	place, _ := client.MakeDecision()
+	fmt.Printf("Eat at %s!\n", place)
 }
