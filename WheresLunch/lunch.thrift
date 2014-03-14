f6b689a068d71dc5e6ebf98fb46ace82b03d02d3
Craig Peterson <cpeterson@ancestry.com>
2014-03-14 11:27:14 -0600

    IDL File

--- WheresLunch/lunch.thrift
+++ WheresLunch/lunch.thrift
@@ -1,2 +1,18 @@
 namespace csharp WheresLunchServer
 namespace go lunch
+
+/** Struct represents a single vote*/
+struct Vote{
+	1: string Name,
+	2: string Place,
+}
+
+/** Means they already voted */
+exception AlreadyVotedException{
+}
+
+service LunchService{
+	string MakeDecision()
+	list<string> GetSuggestions()
+	void PlaceVote(1: Vote v) throws (1: AlreadyVotedException a)
+}
